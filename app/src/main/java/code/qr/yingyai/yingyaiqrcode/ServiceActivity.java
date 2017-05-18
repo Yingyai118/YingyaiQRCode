3b7eb95eee9260f3b27ab623e6e99887fe408d55
Yingyai <yingyai118@gmail.com>
2017-05-18 09:45:14 +0700

    Show text OK

--- app/src/main/java/code/qr/yingyai/yingyaiqrcode/ServiceActivity.java
+++ app/src/main/java/code/qr/yingyai/yingyaiqrcode/ServiceActivity.java
@@ -2,12 +2,39 @@ package code.qr.yingyai.yingyaiqrcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //  Explicit
+    private TextView textView;
+    private ImageView brImageView,qrImageView ;
+    private ListView listView;
+    private String nameString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        //  Initial view
+
+        initialView();
+
+        //Show text
+
+        nameString = getIntent().getStringExtra("Login");
+        textView.setText(nameString);
+
+
     }// Main Method
+
+    private void initialView() {
+        textView = (TextView) findViewById(R.id.txtNameLogin);
+        brImageView = (ImageView) findViewById(R.id.imvBarCode);
+        qrImageView = (ImageView) findViewById(R.id.imvQrCode);
+        listView = (ListView) findViewById(R.id.LivProduct);
+    }
 }   //Main Class
